30d8d17264a162d11616c9b1f89f92bff69f5d41
aestronik <aestronik@gmail.com>
2023-12-03 02:05:18 -0700

    Learned about Generics

--- src/main.zig
+++ src/main.zig
@@ -34,9 +34,41 @@ const Palette = struct {
     };
 };
 
+fn Component_Entity(comptime Generic_Entity: type) type {
+    return struct {
+        in_use: bool,
+        data: Generic_Entity,
+    };
+}
+
+fn Component_List(comptime Generic_Type: type) type {
+    return struct {
+        List: []Generic_Type,
+        length: usize,
+        final_index: usize,
+    };
+}
+
+fn add (comptime T: type, container: Component_List(Component_Entity(T)), entity: T) void {
+    container.List[20] = Component_Entity(T) {
+        .in_use = true,
+        .data = entity
+    };
+}
+
 pub fn main() !void {
     const screen_width = 800;
     const screen_height = 450;
+
+    var test_array: [0x100]Component_Entity(i32) = undefined;
+
+    var test_container = Component_List(Component_Entity(i32)) {
+        .List = &test_array,
+        .length = 0x100,
+        .final_index = 0
+    };
+
+    add(i32, test_container, 400);
 
     raylib.InitWindow(screen_width, screen_height, "zig_game");
     while (!raylib.WindowShouldClose()) {
@@ -48,7 +80,7 @@ pub fn main() !void {
         const y = raylib.GetMouseY();
         // const color = if (raylib.IsMouseButtonDown(0)) Palette.red else Palette.blue;
         const color = if (raylib.IsKeyDown(raylib.KEY_A)) Palette.red else Palette.blue;
-        raylib.DrawLine(0, 0, x, y, color);
+        raylib.DrawLine(test_container.List[20].data, 0, x, y, color);
         raylib.EndDrawing();
     }
     raylib.CloseWindow();
